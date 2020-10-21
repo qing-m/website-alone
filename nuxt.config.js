ecc5c9563bca0a75220629a6457b394b2eb9c0dc
wangheyao <wangheyao@zealfi.com>
2020-10-21 16:32:37 +0800

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,14 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'website-alone',
+    title: '肥宅日记',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: './static/favicon.ico' }
     ]
   },
 
