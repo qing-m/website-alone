58dfb912ee4848308eef11170778435f8344822f
wangheyao <wangheyao@zealfi.com>
2020-12-08 16:20:40 +0800

    自定义模板 自定义布局

---
+++ layouts/main.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/views/index.vue
+++ pages/views/index.vue
@@ -37,7 +37,7 @@ export default {
       title: 'Views'
     }
   },
-  layout: '',
+  layout: 'main',
   middleware: '',
   scrollToTop: '',
   transitions: '',
