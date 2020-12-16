64f8c1ffbfcfe1399461670123935dff2f2a0eab
wangheyao <wangheyao@zealfi.com>
2020-12-16 11:15:20 +0800

    search

--- components/layout/page-header/page-header.vue
+++ components/layout/page-header/page-header.vue
@@ -19,8 +19,26 @@
             </p>
           </nuxt-link>
         </ul>
+        <div :class="['SearchBar','AppHeader-SearchBar',{'AppHeader-Search-isFocus': isInputFocus}]">
+          <div class="SearchBar-tool">
+            <div class="SearchBar-input Input-wrapper">
+              <el-input
+                v-model="serchVal"
+                suffix-icon="el-icon-search"
+                type="text"
+                placeholder="有哪些别出心裁的情话"
+                maxlength="100"
+                @blur="handleBlurVal"
+                @focus="handleFocusVal"
+              />
+            </div>
+          </div>
+          <el-button class="SearchBar-button" type="primary">
+            提问
+          </el-button>
+        </div>
         <div class="page-header-user-info">
-          <div class="user-info-avatar" />
+          <img class="Avatar" src="https://pic3.zhimg.com/v2-d23a46def3f2047fb8ce095f649a71e7_is.jpg" alt="">
         </div>
       </div>
     </header>
@@ -36,15 +54,29 @@ export default {
       default: () => []
     }
   },
+  data () {
+    return {
+      serchVal: '',
+      isInputFocus: false
+    }
+  },
   computed: {
     fullPath () {
       return this.$route.fullPath
+    }
+  },
+  methods: {
+    handleBlurVal () {
+      this.isInputFocus = false
+    },
+    handleFocusVal () {
+      this.isInputFocus = true
     }
   }
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 @import '@/assets/scss/mixin.scss';
 @import '@/assets/scss/_themes.scss';
 @import '@/assets/scss/_handle.scss';
@@ -116,16 +148,97 @@ export default {
         }
       }
     }
+    .AppHeader-SearchBar {
+      height: 34px;
+      -webkit-box-flex: 1;
+      flex-grow: 1;
+      position: relative;
+      &.SearchBar {
+        display: flex;
+      }
+      &.AppHeader-Search-isFocus {
+        .SearchBar-button {
+          display: none;
+        }
+        .SearchBar-tool {
+          width: 100%;
+          .SearchBar-input {
+            transition: width .2s ease, background .3s ease;
+            width: 100%;
+          }
+        }
+      }
+      .SearchBar-tool {
+        height: 100%;
+        position: relative;
+        overflow: hidden;
+        z-index: 104;
+        .SearchBar-input {
+          flex: 1;
+          box-sizing: border-box;
+          padding-left: 12px;
+          padding-right: 0;
+          width: 260px;
+          .el-input {
+            width: auto;
+            position:relative;
+            height: 24px;
+            line-height: 24px;
+            flex: 1 1;
+            display: flex;
+            .el-input__inner{
+              color: #121212 !important;
+              height: 24px;
+              line-height: 24px;
+              padding: 0 5px 0 0;
+              overflow: hidden;
+              background-color: transparent;
+              border: none;
+              resize: none;
+              font-size: 14px;
+              box-sizing: border-box;
+              &.el-input_inner::placeholder {
+                color: #8590a6;
+              }
+            }
+            .el-input__suffix {
+              position: relative;
+              color: #8590a6;
+              cursor: pointer;
+              .el-input__icon {
+                line-height: 24px;
+              }
+            }
+          }
+          &.Input-wrapper {
+            position: relative;
+            display: flex;
+            -webkit-box-align: center;
+            align-items: center;
+            height: 34px;
+            padding: 4px 10px;
+            font-size: 14px;
+            border: 1px solid #ebebeb;
+            border-radius: 3px;
+            background-color: #F6F6F6;
+          }
+        }
+      }
+      .SearchBar-button {
+        margin-left: 16px;
+        padding: 0 14px;
+        line-height: 30px;
+      }
+    }
     .page-header-user-info {
       display: flex;
       align-items: center;
       flex-direction: row-reverse;
-      flex: 1;
+      flex: 4;
       text-align: right;
-      .user-info-avatar {
+      .Avatar {
         width: 30px;
         height: 30px;
-        background-color: turquoise;
       }
     }
   }
