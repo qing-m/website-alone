d8f9ca15cc4483fa8c40278070c5fdf1d28f67b4
wangheyao <wangheyao@zealfi.com>
2021-03-01 15:44:18 +0800

    fix

--- components/layout/page-main/GlobalSideBar/CreatorEntrance.vue
+++ components/layout/page-main/GlobalSideBar/CreatorEntrance.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="CreatorEntrance">
-      <nuxt-link to="" tag="div" class="CreatorEntrance-link CreatorEntrance-Link--smallIcon">
+      <nuxt-link to="" class="CreatorEntrance-link CreatorEntrance-Link--smallIcon">
         <div class="CreatorEntrance-hint ProfileSideCreator-creatorEntranceHint CreatorEntrancePage-hint--smallIcon">
           <svg class="Zi Zi--Creator CreatorEntrance-creatorIcon" fill="currentColor" viewBox="0 0 24 24" width="24" height="24"><path d="M15.075 15.388l-3.024 3.024a4.041 4.041 0 0 0-1.014 1.697l-.26.868C7.844 20.986 4.91 21 2 21c.026-3.325 0-3.304.59-3.956 1.237-1.368 6.251-.68 6.44-2.976.043-.518-.36-1.06-.725-1.69C6.285 8.87 5.512 2 11.5 2c5.988 0 5.15 7.072 3.246 10.378-.357.62-.795 1.217-.724 1.77.073.571.477.958 1.053 1.24zm5.402 1.672c.523.55.523.646.523 3.94a535.11 535.11 0 0 0-4.434-.028l3.911-3.912zm-7.88 2.699c.111-.37.312-.705.584-.978l4.76-4.76a.291.291 0 0 1 .412 0l1.626 1.626a.291.291 0 0 1 0 .411l-4.76 4.76c-.272.273-.608.474-.978.585l-1.96.588a.219.219 0 0 1-.272-.272l.589-1.96zm9.157-6.742a.839.839 0 0 1 0 1.187l-.94.94a.28.28 0 0 1-.395 0l-1.563-1.563a.28.28 0 0 1 0-.395l.94-.94a.839.839 0 0 1 1.187 0l.771.771z" fill-rule="evenodd" /></svg>
           <div class="CreatorEntrance-text">
@@ -48,7 +48,7 @@
           </div>
         </div>
         <div class="css-19mtex">
-          <nuxt-link to="" tag="div" class="css-1qzrmsu">
+          <nuxt-link to="" class="css-1qzrmsu">
             瓦力保镖为你的创作提供持续保护
             <span>
               <svg width="17" height="17" viewBox="0 0 16 16" class="css-14gt3bc" fill="currentColor"><path d="M8 3l.737.339.428.187.377.158.326.127c.15.055.282.1.395.133.55.161 1.297.157 2.237-.014l-.002 3.784c0 3.07-2.52 4.76-4.498 5.286-1.977-.527-4.498-2.216-4.498-5.286L3.5 3.93c.94.17 1.686.175 2.237.014C6.187 3.812 6.941 3.497 8 3zm2.38 3.066L7.599 8.834 6.32 7.566a.4.4 0 0 0-.554.554l1.55 1.55a.5.5 0 0 0 .568 0l3.05-3.05a.4.4 0 0 0-.554-.554z" /></svg>
