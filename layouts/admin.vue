8f91a01e9a2cb6c62f78818bc5a2740c26f2bdf0
wangheyao <wangheyao@zealfi.com>
2021-07-09 14:40:27 +0800

    设计ui

---
+++ layouts/admin.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="adminComponentContainer">
+    <div class="adminCardContainer">
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+.adminComponentContainer {
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #0E0E0E;
+  .adminCardContainer {
+    width: 200px;
+    height: 65%;
+    border-radius: 10px;
+    background: #FEF900;
+  }
+}
+</style>
---
+++ pages/register/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="registerContainer">
+
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'admin'
+}
+</script>
+
+<style lang="scss" scoped>
+.registerContainer {
+  .title {
+    color: #1F1E0D;
+    text-align: center;
+    padding-top: 10px;
+  }
+}
+</style>
