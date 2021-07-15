52f64105fc502048c3eaf5fccf9fab67d396d3ab
wangheyao <1874373481@qq.com>
2021-07-15 17:36:38 +0800

    重新设计UI

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -1,20 +1,6 @@
 <template>
   <div class="adminComponentContainer">
-    <div class="adminNavContainer">
-      <nuxt-link to="/register">
-        <NeonText data-text='REGISTER' />
-      </nuxt-link>
-      <nuxt-link to="/login">
-        <NeonText data-text='LOGIN' />
-      </nuxt-link>
-    </div>
     <div class="adminCardContainer">
-      <div class="cardLeft">
-        <img src="~@/assets/image/admin/admin-bg.png" alt="">
-      </div>
-      <div class="cardRight">
-        <Nuxt />
-      </div>
     </div>
   </div>
 </template>
@@ -33,37 +19,6 @@ export default {
   width: 100%;
   height: 100vh;
   background: #000000;
-  padding: 30px;
   box-sizing: border-box;
-  .adminNavContainer {
-    width: 1160px;
-    margin: 0 auto;
-    display: flex;
-    align-items: center;
-    /deep/.neonTextContainer {
-      margin-right: 20px;
-      .neon {
-        font-size: 60px !important;
-      }
-    }
-  }
-  .adminCardContainer {
-    width: 1160px;
-    margin: 0 auto;
-    height: calc(100% - 120px);
-    border: 1px solid #FFFFFF;
-    box-shadow: 0px 0px 20px #FFFFFF;
-    display: flex;
-    .cardLeft {
-      width: 50%;
-      img {
-        width: 100%;
-        height: 100%;
-      }
-    }
-    .cardRight {
-      width: 50%;
-    }
-  }
 }
 </style>
