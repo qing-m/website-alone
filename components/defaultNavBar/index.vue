0270b5ec2abf6855adc1220510d96969c166f670
wangheyao <1874373481@qq.com>
2021-08-03 14:59:57 +0800

    nav

--- components/defaultNavBar/index.vue
+++ components/defaultNavBar/index.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="defaultNavBar">
     <div class="container">
-      Hello Nav
+      <div class="user-avatar-name">
+        <img src="https://outin-ede8be7da1c311ebbe1400163e1c35d5.oss-cn-shanghai.aliyuncs.com/image/default/C0D3C14D40EA4C23A005032E0E5E3887-6-2.png">
+        <p>Alone</p>
+      </div>
     </div>
   </div>
 </template>
@@ -17,8 +20,29 @@ export default {
   width: 100%;
   height: 44px;
   box-sizing: border-box;
-  padding-top: 58px;
-  background: #17A0F7;
+  background-image: linear-gradient(90deg,#51CBFC, #47A7FE);
   position: relative;
+  padding: 0 16px;
+  user-select: none;
+  .container {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    .user-avatar-name {
+      display: flex;
+      align-items: center;
+      img {
+        width: 30px;
+        height: 30px;
+        border-radius: 50%;
+        margin-right: 5px;
+      }
+      p {
+        font-size: 16px;
+        color: #FFFFFF;
+      }
+    }
+  }
 }
 </style>
